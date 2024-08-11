7efde5e95fbfb5201e27a049865b623fb9e81932
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-12 01:22:30 +0530

    update paths

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 interface User {
   id: number;
@@ -6,17 +6,22 @@ interface User {
 }
 
 const UsersPage = async () => {
-const res = await fetch('https://jsonplaceholder.typicode.com/users')
-const users: User[] = await res.json();
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: 'no-store',
+  });
+  const users: User[] = await res.json();
 
   return (
     <div>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
