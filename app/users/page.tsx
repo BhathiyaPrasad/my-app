7c90528e663387a8bd72c434c0ce21dbb9fff44b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-12 02:11:10 +0530

    added daisyui styled table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,23 +3,36 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: 'no-store',
+    cache: "no-store",
   });
   const users: User[] = await res.json();
 
   return (
     <div>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {/* <p>{new Date().toLocaleTimeString()}</p> */}
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              {user.email}
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
